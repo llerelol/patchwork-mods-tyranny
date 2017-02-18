c97d364d8e4a86af87f38a555397abd9db5cb8e3
llerelol <petr.komogortsev@gmail.com>
2017-02-18 14:11:21 +0300

    initial commit

---
+++ TyrGameInfo/TyrGameInfo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patchwork.AutoPatching;
+
+namespace TyrGameInfo
+{
+    [AppInfoFactory]
+    internal class TyrAppInfoFactory : AppInfoFactory
+    {
+        public override AppInfo CreateInfo(DirectoryInfo folderInfo)
+        {
+
+            string exeFileName;
+            string exeFileName64;
+            string iconFile;
+            string appVersion;
+
+            bool multiarch = true;
+
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                exeFileName = "Tyranny";
+                exeFileName64 = ""; //no idea
+                iconFile = "Tyranny.png";
+                appVersion = null;
+            }
+            else
+            {
+                exeFileName = iconFile = "Tyranny32.exe";
+                exeFileName64 = iconFile = "Tyranny64.exe";
+
+                appVersion = FileVersionInfo.GetVersionInfo(Path.Combine(folderInfo.FullName, exeFileName)).FileVersion;
+            }
+
+            var fileInfos = folderInfo.GetFiles(exeFileName).ToList();
+
+            if (fileInfos.Count == 0)
+            {
+                throw new FileNotFoundException($"Tyranny's executable file '{exeFileName}' was not found in this directory.", exeFileName);
+            }
+
+            if (multiarch)
+            {
+                fileInfos.AddRange(folderInfo.GetFiles(exeFileName64).ToList());
+
+                if (fileInfos.Count < 2)
+                {
+                    throw new FileNotFoundException($"Tyranny's executable file '{exeFileName64}' was not found in this directory.", exeFileName64);
+                }
+            }
+
+            return new AppInfo()
+            {
+                BaseDirectory = folderInfo,
+                Executable = fileInfos[0],
+                Executable64 = (fileInfos.Count == 2) ? fileInfos[1] : null,
+                AppVersion = appVersion,
+                AppName = "Tyranny",
+                MultiArch = multiarch,                
+                IconLocation = new FileInfo(Path.Combine(folderInfo.FullName, iconFile)),
+                IgnorePEVerifyErrors = new[] {
+			        //Expected an ObjRef on the stack.(Error: 0x8013185E). 
+			        //-you can ignore the following. They are present in the original DLL. I'm not sure if they are actually errors.
+			        0x8013185EL,
+			        //The 'this' parameter to the call must be the calling method's 'this' parameter.(Error: 0x801318E1)
+			        //-this isn't really an issue. PEV is just confused.
+			        0x801318E1,
+			        //Call to .ctor only allowed to initialize this pointer from within a .ctor. Try newobj.(Error: 0x801318BF)
+			        //-this is a *verificaiton* issue is caused by copying the code from an existing constructor to a non-constructor method 
+			        //-it contains a call to .ctor(), which is illegal from a non-constructor method.
+			        //-There will be an option to fix this at some point, but it's not really an error.
+			        0x801318BF,
+                }
+            };
+        }
+    }
+}
---
+++ modules/TrainerFix.pw/main.cs
@@ -0,0 +1,158 @@
+﻿using Patchwork;
+using Game;
+using UnityEngine;
+
+namespace TrainerFix.pw
+{
+    [ModifiesType]
+    public class mod_CharacterStats : CharacterStats
+    {
+        [ModifiesMember("LevelUpTo")]
+        public void LevelUpToNew(int level, bool naturalProgression)
+        {
+            Player component = base.GetComponent<Player>();
+            bool flag = !component && !StoredCharacterInfo.RestoringPackedCharacter && GameState.Mode.Option.GetOption(GameOption.BoolOption.AUTO_LEVEL_COMPANIONS);
+            int level2 = this.Level;
+            while (this.Level < level)
+            {
+                this.Level++;
+                this.GainLevelUpPoints(this.Level);
+                if (flag)
+                {
+                    this.AutoLevelAttributes(this.Level);
+                }
+            }
+            if (level2 != level)
+            {
+                this.AbilityProgressionTable.AddAutoGrantAbilitiesToCharacter(this);
+                if (flag)
+                {
+                    this.AutoLevelTalents();
+                }
+                if (naturalProgression)
+                {
+                    this.OnLevelUp(this, null);
+                }
+                //this.m_numSkillsTrainedThisLevel = 0;
+            }
+        }
+    }
+    
+    [ModifiesType]
+    public class mod_UISkillTrainingManager : UISkillTrainingManager
+    {
+        [ModifiesMember("LoadSelectedCharacterInformation")]
+        private void LoadSelectedCharacterInformationNew()
+        {
+            this.LblCharacterName.text = this.m_selectedCharacter.Name();
+            this.LblCharacterLvl.text = string.Format(SDK.GUIUtils.GetText(1107), this.m_selectedCharacter.Level);
+            this.LblTrainingsThisLevel.text = string.Format(SDK.GUIUtils.GetText(2123), this.m_selectedCharacter.SkillsTrainedThisLevel, this.m_selectedCharacter.Level * 5);
+            int num = CharacterStats.ExperienceNeededForLevel(this.m_selectedCharacter.Level);
+            int num2 = CharacterStats.ExperienceNeededForNextLevel(this.m_selectedCharacter.Level);
+            int num3 = this.m_selectedCharacter.Experience - num;
+            int num4 = num2 - num;
+            this.LblProgressXP.text = string.Format(SDK.GUIUtils.GetText(291), num3, num4);
+            float num5 = (float)num3 / (float)num4;
+            Vector3 localScale = this.ProgressSize.transform.localScale;
+            localScale.x = num5 * localScale.x;
+            this.ProgressLevelXP.transform.localScale = localScale;
+        }
+
+        [ModifiesMember("OnTrainButtonClicked")]
+        private void OnTrainButtonClickedNew(UISkillTrainingUpgrade upgradeView)
+        {
+            Game.CharacterStats.SkillType skillType = upgradeView.SkillType;
+            float skillPerRankMultiplier = StatsData.Instance.GetSkillPerRankMultiplier(skillType);
+            int skillRank = this.m_selectedCharacter.GetSkillRank(skillType);
+            int num = skillRank + 1;
+            if (num > this.m_trainer.maxSkillLevel)
+            {
+                UIMessageBox uIMessageBox = UIWindowManager.ShowMessageBox(UIMessageBox.UIDialogButtons.OK, SDK.GUIUtils.GetText(2131), string.Format(SDK.GUIUtils.GetText(2129), this.m_trainer.maxSkillLevel));
+                uIMessageBox.ShowWindow();
+                return;
+            }
+            if (this.m_selectedCharacter.SkillsTrainedThisLevel >= this.m_selectedCharacter.Level * 5)
+            {
+                UIMessageBox uIMessageBox2 = UIWindowManager.ShowMessageBox(UIMessageBox.UIDialogButtons.OK, SDK.GUIUtils.GetText(2131), SDK.GUIUtils.GetText(2132));
+                uIMessageBox2.ShowWindow();
+                return;
+            }
+            int num2 = Game.CharacterStats.CopperCostToTrainSkillToNextRank(num);
+            Game.PlayerInventory component = Game.GameState.s_playerCharacter.GetComponent<Game.PlayerInventory>();
+            if (component == null || component.currencyTotalValue < (float)num2)
+            {
+                UIMessageBox uIMessageBox3 = UIWindowManager.ShowMessageBox(UIMessageBox.UIDialogButtons.OK, SDK.GUIUtils.GetText(2131), SDK.GUIUtils.GetText(2128));
+                uIMessageBox3.ShowWindow();
+                return;
+            }
+            if (Game.TelemetryManager.Instance && Game.Stronghold.Instance)
+            {
+                Game.TelemetryManager.Instance.QueueEvent_SkyPillarUpgradeUsed(SDK.TelemetryManager.Destination.Developer | SDK.TelemetryManager.Destination.Publisher, Game.Stronghold.Instance.GetUpgradeLocation(StrongholdUpgrade.Type.Weapons_Training), StrongholdUpgrade.Type.Weapons_Training, string.Empty, skillType);
+            }
+            GlobalAudioPlayer.SPlay(UIAudioList.UIAudioType.TrainSkill);
+            int xp = Game.CharacterStats.ExperienceNeededForNextSkillRank(skillRank, skillPerRankMultiplier) - this.m_selectedCharacter.SkillXP[(int)skillType];
+            component.RemoveCurrency((float)num2, 1);
+            this.m_selectedCharacter.SkillsTrainedThisLevel++;
+            this.m_selectedCharacter.CalculateAndApplySkillXP(upgradeView.SkillType, xp, null, 0, 0);
+            this.Reload();
+        }
+    }
+
+    [ModifiesType]
+    public class mod_UISkillTrainingUpgrade : UISkillTrainingUpgrade
+    {
+        [ModifiesMember("LoadSkill")]
+        public void LoadSkillNew(Game.CharacterStats character, Game.CharacterStats.SkillType skillToLoad, int trainerMaxRank)
+        {
+            this.m_trainFailReason = string.Empty;
+            this.m_skillType = skillToLoad;
+            this.LblName.text = Game.GUIUtils.GetSkillTypeString(this.m_skillType);
+            bool flag = false;
+            float skillPerRankMultiplier = StatsData.Instance.GetSkillPerRankMultiplier(this.m_skillType);
+            int skillRank = character.GetSkillRank(this.m_skillType);
+            this.m_skillXP = character.SkillXP[(int)this.m_skillType];
+            this.m_nextRankXP = Game.CharacterStats.ExperienceNeededForNextSkillRank(skillRank, skillPerRankMultiplier);
+            this.LblRank.text = string.Format(SDK.GUIUtils.GetText(291), skillRank, trainerMaxRank);
+            this.LblRank.color = UIGlobalColor.FetchColor(UIGlobalColor.ColorLookupID.TEXT_NORMAL_DEFAULT);
+            this.ProgressRank.fillAmount = (float)this.m_skillXP / (float)this.m_nextRankXP;
+            if (skillRank + 1 > trainerMaxRank)
+            {
+                this.m_trainFailReason = string.Format(SDK.GUIUtils.GetText(2129), trainerMaxRank);
+                this.LblRank.color = UIGlobalColor.FetchColor(UIGlobalColor.ColorLookupID.TEXT_UNAFFORDABLE);
+                flag = true;
+            }
+            if (character.SkillsTrainedThisLevel >= character.Level * 5)
+            {
+                this.m_trainFailReason = SDK.GUIUtils.GetText(2132);
+            }
+            int num = Game.CharacterStats.CopperCostToTrainSkillToNextRank(skillRank + 1);
+            Game.Player s_playerCharacter = Game.GameState.s_playerCharacter;
+            if (s_playerCharacter == null)
+            {
+                return;
+            }
+            Game.PlayerInventory component = s_playerCharacter.GetComponent<Game.PlayerInventory>();
+            if (component == null)
+            {
+                return;
+            }
+            bool flag2 = component.currencyTotalValue >= (float)num;
+            this.CostView.ShowCost(num, flag2);
+            if (!flag2)
+            {
+                this.m_trainFailReason = SDK.GUIUtils.GetText(2128);
+            }
+            this.ButtonMain.IsEnabled = (this.m_trainFailReason.Length == 0);
+            if (!flag && !this.ButtonMain.IsEnabled)
+            {
+                this.LblRank.color = UIGlobalColor.FetchColor(UIGlobalColor.ColorLookupID.TEXT_NORMAL_DISABLED);
+            }
+            if (this.m_isHovered && this.m_trainFailReason.Length != 0)
+            {
+                this.OnTrainSkillHovered(this.ButtonTrain.gameObject, true);
+            }
+        }
+
+    }
+
+}
